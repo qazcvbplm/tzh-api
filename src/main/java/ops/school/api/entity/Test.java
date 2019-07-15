7f3b569aaf61ecf180a6cca10ba0b4423071f133
tong'zhu'he <tongzhuhe@lidig.com>
2019-07-15 14:52:08 +0800

    增加字段

---
+++ src/main/java/ops/school/api/entity/Test.java
@@ -0,0 +1,38 @@
+package ops.school.api.entity;
+
+import java.math.BigDecimal;
+
+public class Test {
+
+
+    private Integer id;
+
+
+    private String name;
+
+    private BigDecimal amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
